5146f4a9947a4a7694b964426393622a21c4cd57
Jon Udell <jon@jonudell.info>
2015-04-18 18:31:23 -0700

    'ongoing'

--- h.py
+++ h.py
@@ -18,6 +18,10 @@ class GetHandler(BaseHTTPRequestHandler):
         if method == 'activity':
             self.wfile.write(activity(q))
             return;
+        if method == 'user_urls':
+            self.wfile.write(user_urls(q))
+            return;
+
 
 def json2atom(q):
     tag = urlparse.parse_qs(q)['tag'][0]
@@ -31,6 +35,14 @@ def activity(q):
     s = urllib2.urlopen(h_url).read()
     j = json.loads(s)
     return make_activity(j)
+
+def user_urls(q):
+    user = urlparse.parse_qs(q)['user'][0]
+    h_url = 'https://hypothes.is/api/search?limit=1000&user=' + user
+    s = urllib2.urlopen(h_url).read()
+    j = json.loads(s)
+    return make_user_urls(j, user)
+    
 
 def make_activity(j):
     users = {}
@@ -83,6 +95,63 @@ def make_feed(j, tag):
         fe.content(r['uri'])
     return fg.atom_str(pretty=True)
 
+def make_user_urls(j, user):
+    urls = {}
+    texts = {}
+    titles = {}
+    datetimes = {}
+    tags = {}
+    s = '<h1>urls annotated by %s</h1>' % user
+    for row in j['rows']:
+        url = row['uri'].replace('https://via.hypothes.is/h/','')
+        if url.startswith('urn:'):
+            continue
+        add_or_increment(urls, url)
+        datetimes[url] = row['updated']
+        try:
+            title = row['document']['title']
+            if ( isinstance(title, list)):
+                titles[url] = title[0]
+            else:
+                titles[url] = title
+        except:
+            titles[url] = url # it's a reply?
+        try:
+            text = row['text']
+            if len(text):
+                add_or_append(texts, url, text)
+        except:
+            pass
+        try:
+            if len(row['tags']):
+                for t in row['tags']:
+                    add_or_append(tags, url, t)
+        except:
+            pass
+
+    date_ordered_urls = datetimes.keys()
+    date_ordered_urls.reverse()
+
+    for url in date_ordered_urls:
+        taglist = '</i>, <i>'.join(tags[url])  if tags.has_key(url) else ''
+        s += '<p><a href="https://via.hypothes.is/h/%s">%s</a> <i>%s</i></p>' % ( url, titles[url], taglist)
+        if texts.has_key(url):
+            list_of_texts = texts[url]
+            list_of_texts.reverse()
+            for text in list_of_texts: 
+                s += '<blockquote>%s</blockquote>' % text
+    s = """<html>
+<head>
+<style>
+body { font-family: verdana; margin: .5in }
+</style>
+<body>
+%s
+</body>
+</html>
+""" % (s)
+    return s.encode('utf-8')
+
 def add_or_increment(dict, key):
     if dict.has_key(key):
         dict[key] += 1
@@ -90,14 +159,28 @@ def add_or_increment(dict, key):
         dict[key] = 1
     return dict
 
+def add_or_append(dict, key, val):
+    if dict.has_key(key):
+        dict[key].append(val)
+    else:
+        dict[key] = [val]
+    return dict
+    
+
 if __name__ == '__main__':
     from BaseHTTPServer import HTTPServer
+#    server = HTTPServer(('h.jonudell.info', 8080), GetHandler)
     server = HTTPServer(('', 8080), GetHandler)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
 
+####
+for row in j['rows']:
+  uri = row['uri'].replace('https://via.hypothes.is/h/','')
+  add_or_increment(urls, uri)
 
  
+
 
   
  
