285be79748ef650d44248f9823aa771174fef43c
Jon Udell <jon@jonudell.info>
2015-06-21 15:03:51 -0700

    update .gitignore

--- h.js
+++
@@ -1,5 +0,0 @@
-﻿function embed_conversation(id) {
-    element = document.getElementById(id);
-    element.outerHTML = '<iframe height="300" src="https://hypothes.is/a/' + id + '"/>'
-    return false;
-}
